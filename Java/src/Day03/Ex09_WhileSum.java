4821903e8941212e1535c7a93263b965b87efb32
koko2807 <0705ldh@hanmail.net>
2024-08-13 09:42:20 +0900

    실습코드

---
+++ Java/src/Day03/Ex09_WhileSum.java
@@ -0,0 +1,29 @@
+package Day03;
+
+public class Ex09_WhileSum {
+
+	public static void main(String[] args) {
+		// 1부ㅠ터 1000까지 합계를 구하시오.
+		// 1+2+3+... +1000 = ????
+		
+		int a = 1;
+		int sum = 0;
+		// a : 1 2 3 4 ... 1000
+		// sum = sum + a     : ????
+		//		  ?  + 1
+		//
+		
+		while( a <= 1000 ) {
+			// 방법 1
+//			sum = sum + a;
+//			a = a + 1;
+			
+			//방법 2
+//			sum = sum + a++;
+			
+			//방법 3
+			sum += a++;
+		}
+		System.out.println("합계 : " + sum);
+	}
+}

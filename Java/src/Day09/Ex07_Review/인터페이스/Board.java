1c73039ca9bbcc1995c5284511790a8930013dfa
0705ldh <tj-bu-706-21@DESKTOP-6ECML8P>
2024-08-20 17:50:17 +0900

    2024-08-20

--- Java/src/Day09/Ex07_Review/인터페이스/Board.java
+++ Java/src/Day09/Ex07_Review/인터페이스/Board.java
@@ -1,20 +1,20 @@
 package Day09.Ex07_Review.인터페이스;
 
 /**
- *  게시글 정보
- *  - 게시글 번호
- *  - 제목
- *  - 작성자
- *  - 내용
- *  - 등록일자
- *  - 수정일자
+ *	게시글 정보
+ *	- 게시글 번호
+ *	- 제목
+ *	- 작성자
+ *	- 내용
+ *	- 등록일자
+ *	- 수정일자
  */
 public class Board {
-
+	
 	// 멤버 변수 (필드)
 	private int no;
 	private String title;
-	private String Writer;
+	private String writer;
 	private String content;
 	private String regDate;
 	private String updDate;
@@ -26,14 +26,23 @@ public class Board {
 	// 매개변수가 있는 생성자
 	public Board(String title, String writer, String content) {
 		this.title = title;
-		this.Writer = writer;
+		this.writer = writer;
 		this.content = content;
 	}
+	// 전체 매개변수 생성자
+	public Board(int no, String title, String writer, String content, String regDate, String updDate) {
+		this.no = no;
+		this.title = title;
+		this.writer = writer;
+		this.content = content;
+		this.regDate = regDate;
+		this.updDate = updDate;
+	}
+	
 	// getter, setter
 	public int getNo() {
 		return no;
 	}
-
 	public void setNo(int no) {
 		this.no = no;
 	}
@@ -44,10 +53,10 @@ public class Board {
 		this.title = title;
 	}
 	public String getWriter() {
-		return Writer;
+		return writer;
 	}
 	public void setWriter(String writer) {
-		Writer = writer;
+		this.writer = writer;
 	}
 	public String getContent() {
 		return content;
@@ -67,18 +76,12 @@ public class Board {
 	public void setUpdDate(String updDate) {
 		this.updDate = updDate;
 	}
+	
 	// toString()
 	@Override
 	public String toString() {
-		return "Board [no=" + no + ", title=" + title + ", Writer=" + Writer + ", content=" + content + ", regDate="
+		return "Board [no=" + no + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
 				+ regDate + ", updDate=" + updDate + "]";
 	}
-	
+
 }
-
-
-
-
-
-
-
